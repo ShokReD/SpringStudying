4ecbb3c6fd5f8b8923c62126ec72c21757e0d25a
ShokReD <shokred@yandex.ru>
2018-08-17 00:17:44 +0700

    Add parent request mapping

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/SillyController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/SillyController.java
@@ -3,12 +3,13 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-@RequestMapping("/hello")
-public class HomeController {
+@RequestMapping("/silly")
+public class SillyController {
 	
 	@RequestMapping("/showForm")
-	public String showPage() {
-		return "main-menu";
+	public String displayTheForm() {
+		return "silly";
 	}
 }
