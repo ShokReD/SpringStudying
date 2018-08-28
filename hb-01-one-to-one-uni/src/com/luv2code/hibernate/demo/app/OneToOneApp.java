381381e0a9dff0f7397f6c74ed32114e6c199843
ShokReD <shokred@yandex.ru>
2018-08-28 22:31:13 +0700

    Add create query example with join table

---
+++ hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/app/OneToOneApp.java
@@ -0,0 +1,39 @@
+package com.luv2code.hibernate.demo.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class OneToOneApp {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Instructor.class)
+				.buildSessionFactory();
+		
+		try (Session session = factory.getCurrentSession()) {
+			Instructor instructor = new Instructor("john", "wurst", "wurst@mail.com");
+			InstructorDetail detail = new InstructorDetail("wurst_channel", "singing");
+			
+			instructor.setInstructorDetail(detail);
+			
+			session.beginTransaction();
+			
+			// this will also save the details object
+			// cause of CascadeType.ALL
+			System.out.println("Saving instructor: " + instructor);
+			
+			session.save(instructor);
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+	}
+
+}
