fc15dc3c1588b1ca53709e06c686ea0d4f2f4c1e
ShokReD <shokred@yandex.ru>
2018-08-29 23:21:34 +0700

    Add deleting bi-directional example

--- hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/app/DeleteInstructorDetailDemo.java
+++ hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/app/DeleteInstructorDetailDemo.java
@@ -7,7 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
 
-public class GetInstructorDetailDemo {
+public class DeleteInstructorDetailDemo {
 
 	public static void main(String[] args) {
 		SessionFactory factory = new Configuration()
@@ -17,17 +17,16 @@ public class GetInstructorDetailDemo {
 				.buildSessionFactory();
 		
 		try (Session session = factory.getCurrentSession()) {
-			int id = 9999;
+			int id = 2;
 			
 			session.beginTransaction();
 			
 			InstructorDetail detail = session.get(InstructorDetail.class, id); 
-			System.out.println("Found instructor: " + detail);
+			System.out.println("Found instructorDetail: " + detail);
 			
 			if (detail != null) {
-				System.out.println("Selecting detail: " + detail);
-				System.out.println("Selecting instructor: " + detail.getInstructor());
-//				session.delete(detail);
+				System.out.println("Deleting: " + detail);
+				session.delete(detail);
 			}
 			
 			session.getTransaction().commit();
